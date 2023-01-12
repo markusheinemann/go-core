1208e9249dbbf83be1b2aeef47192d4ee9d661aa
Markus Heinemann <markus@heinemann.dev>
2023-01-12 02:38:19 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# go-core
+
+This repository contains some utility functions that I use across several golang projects in my daily work.
+
+## Features
+
+* http
+  * Read a JSON string from the body into a struct
+  * Write a struct as JSON response
+  * Retrieve client ip address (works behind cloudflare and other proxies)
+* strings
+  * Generate alphanumeric strings very performant
+
+## ToDos
+
+- [ ] Add examples
+- [ ] Add tests
+- [ ] Write a documentation
+- [ ] Add more functionality
---
+++ src/strings.go
@@ -0,0 +1,39 @@
+package core
+
+import (
+	"math/rand"
+	"time"
+	"unsafe"
+)
+
+type stringsT struct{}
+
+var Strings stringsT
+
+const (
+	letterIdxBits = 6
+	letterIdxMask = 1<<letterIdxBits - 1
+	letterIdxMax  = 63 / letterIdxBits
+)
+
+var src = rand.NewSource(time.Now().UnixNano())
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+// GenerateAlphaNumeric generates a random alphanumeric string with the length of n
+func (s *stringsT) GenerateAlphaNumeric(n int) string {
+	b := make([]byte, n)
+
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+	return *(*string)(unsafe.Pointer(&b))
+}
